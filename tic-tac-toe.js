9ca287a9996637c23950bb78a75d28ecea115705
Brandon <omstachu626@gmail.com>
2021-04-29 15:03:24 -0700

    stuck on

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,20 +1,43 @@
 window.addEventListener('DOMContentLoaded', event => {
     const board = document.getElementById('tic-tac-toe-board');
-
+    let player = 0;
 
     board.addEventListener('click', event => {
-            let eti = event.target.id
-            if (eti === 'tic-tac-toe-board') {
+        let eti = event.target.id
+        let et = event.target;
+
+        if (eti === 'tic-tac-toe-board') {
+            // console.log(eti)
+        } else {
+            // const elem = eti;
+
+            const xImage = document.createElement('img', )
+            xImage.src = 'https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg'
+
+            // console.log(xImage.src)
+            const oImage = document.createElement('img', )
+            oImage.src = 'https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg'
                 // console.log(eti)
+
+
+            // et.appendChild(xImage);
+            console.log(Object.values(et));
+            // console.log(document.querySelectorAll("played"))
+            // console.log(et.className);
+            // if (et === xImage.src || et === oImage.src) {
+            //     console.log('gut');
+            // }
+            if (!player) {
+                et.appendChild(oImage);
+                // et.classList.add("played")
+                player = 1;
             } else {
-        const elem = eti;
-        const xImage = document.createElement('img',)
-                xImage.src = 'https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg'
-                console.log(xImage.src)
-        const oImage = document.createElement('img',)
-                oImage.src = 'https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg'
-                // console.log(eti)
+                et.appendChild(xImage);
+                player = 0;
+                // et.classList.add("played")
             }
+
+        }
     })
 
 
